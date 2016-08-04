cb471a90c4e33594df2a1419f8d890b822a8e7d5
namhoai <hoainamak521996@gmail.com>
2016-08-04 09:56:09 +0700

    update

---
+++ tong.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x;
+int i;
+int tong;
+    for (int i=1;i<=10;i++)
+    {
+    	cin>>x;
+    	tong=tong+x;
+	}
+	float tb;
+	tb=tong/(i-1);
+	cout<<"gia tri tb ="<<tb;
+	return 0;
+}
+
